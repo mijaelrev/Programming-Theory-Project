8d02ddd1e32df15934a3fdceda33459751d5367d
mijaelrev <mijael_qwert@hotmail.com>
2021-06-26 00:42:46 -0500

    Cambios: Creando un bloque nuevo Modificando algunos

---
+++ Assets/Code/ElementsManager/CloudBlock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Threading.Tasks;
+
+namespace Code.ElementsManager
+{
+    public class CloudBlock : Block
+    {
+
+        private void OnCollisionEnter(Collision collision) => CollisionAction(collision);
+        protected override void TriggerAction(Collider other) { }
+
+        internal override void CollisionAction(Collision collision)
+        {
+            if (!collision.gameObject.CompareTag("Player")) return;
+            CloudDisolve();
+        }
+        private async void CloudDisolve()
+        {
+            await Task.Delay(System.TimeSpan.FromSeconds(_touchTimeRecycle));
+            Recycle();
+        }
+
+        internal override void ReInit() => Invoke(nameof(Recycle), _timeRecycle);
+        internal override void Release() => Debug.Log("CloudBlock Recycle");
+    }
+}
--- Assets/Code/ElementsManager/LavaBlock.cs
+++ Assets/Code/ElementsManager/LavaBlock.cs
@@ -1,43 +1,44 @@
 ﻿using UnityEngine;
-using System.Threading.Tasks;
-using System.Threading;
 
 namespace Code.ElementsManager
 {
     public class LavaBlock : Block
     {
-        private CancellationTokenSource _cancelToken = new CancellationTokenSource();
         [SerializeField] private float _timeKill = 1f;
-        [SerializeField] private float _currentTime;
-        private void OnCollisionEnter(Collision collision)
+        private float _currentTime;
+        private bool _mustKill = false;
+
+        private void Update() => KillThePlayer();
+
+        private void KillThePlayer()
         {
-            CollisionAction(collision);
+            if (_mustKill)
+            {
+                _currentTime -= Time.deltaTime;
+
+                if (_currentTime <= 0)
+                {
+                    PlayerKill();
+                    _currentTime = 0;
+                }
+            }
+            else
+            {
+                _currentTime = _timeKill;
+            }
         }
 
-        private void OnCollisionExit(Collision collision)
-        {
-            _cancelToken?.Cancel();
-            //Task.Run(() => CollisionAction(collision), _cancelToken.Token);
-            //Task.FromCanceled(_cancelToken);
-        }
-        protected override void TriggerAction(Collider other)
-        {
-
-        }
-        internal async override void CollisionAction(Collision collision)
+        private void OnCollisionEnter(Collision collision) => CollisionAction(collision);
+        private void OnCollisionExit(Collision collision) => CollisionAction(collision);
+        protected override void TriggerAction(Collider other) { }
+        internal override void CollisionAction(Collision collision)
         {
             if (!collision.gameObject.CompareTag("Player")) return;
-            Debug.Log("Inicio de la asincronia");
-            await Task.Delay(System.TimeSpan.FromSeconds(_timeKill));
-            Debug.Log("Tiempo de espera terminado");
-            if (_cancelToken.IsCancellationRequested) return;
-            PlayerKill();
-            Debug.Log("Asincronia ejecutada");
+            _mustKill = !_mustKill;
         }
 
         internal override void ReInit() => Invoke(nameof(Recycle), _timeRecycle);
-
-        internal override void Release() => Debug.Log("Lava Reciclado");
+        internal override void Release() => Debug.Log("LavaBlock Recycle");
 
     }
 }
